341eacbeac34a960022a5adbaab8ef4e65b30ced
diego.arioza <diegoariozza@gmail.com>
2019-01-10 22:43:14 -0200

    criando primeiros testes

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """ '/' must return 200 """
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """ must return index.html """
+        self.assertTemplateUsed(self.response, 'index.html')
